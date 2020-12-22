1aef658b05a79e02db118ecc67bc08330f855c29
Marco <marco.chianese93@gmail.com>
2020-12-22 17:23:52 +0100

    inizio implementazione contenuti statica

---
+++ resources/views/prodotti.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <img src="{{asset('marchio-sito-test.png')}}" alt="">
+            <ul>
+                <li>Home</li>
+                <li>prodotti</li>
+                <li>News</li>
+            </ul>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
